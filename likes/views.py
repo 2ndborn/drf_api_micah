f194d3be2576dfa1c078e19e605c10e7eaf82bbf
2ndborn <micah.francis69@googlemail.com>
2025-04-19 14:03:50 +0000

    created the likes generics views and urls

--- likes/views.py
+++ likes/views.py
@@ -5,9 +5,22 @@ from .serializers import LikeSerializer
 
 
 class LikeList(generics.ListCreateAPIView):
+    """
+    List likes or create a like if logged in.
+    """
     serializer_class = LikeSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Like.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like. No Update view, as users can only like or unlike a post. 
+    Destroy a like, i.e. unlike a post if owner of that like
+    """
+    serializer_class = LikeSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Like.objects.all()
